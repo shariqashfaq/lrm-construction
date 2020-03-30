48f494f0e551a54775f653890cb6eb2c112c5195
root <root@SHARIQ-LENOVO.localdomain>
2020-03-30 17:49:42 -0400

    implemented image gallery

--- application.py
+++ application.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, jsonify
+import json
 
 app = Flask(__name__)
 
@@ -10,3 +12,17 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 def index():
 
     return render_template("index.html")
+
+#gallery
+@app.route("/slideshow/<attribute>")
+def slideshow(attribute):
+    #get name of requested image directory in /images and respond with images within
+    projects = os.listdir('static/images')
+
+    if (attribute in projects):
+        images = os.listdir(f'static/images/{attribute}')
+        
+        return jsonify({"success": True, "project": images})
+
+    else:
+        return jsonify({"success": False})
